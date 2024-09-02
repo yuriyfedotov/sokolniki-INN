1755409be5192bcfa4e1b3ffa87c3fe1543ab044
Y_Fedotov <92554647+yuriyfedotov@users.noreply.github.com>
2024-09-02 13:32:18 +0300

    upload

---
+++ data.js
@@ -0,0 +1,337 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-p",
+      "name": "Унивеpситет",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.06310190243004854,
+        "pitch": -0.141000312816578,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.8703950555722475,
+          "pitch": 0.04080988090218085,
+          "rotation": 0,
+          "target": "1--2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1--2-",
+      "name": "Холл 2 этаж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.9344224302985129,
+        "pitch": 0.024114263776226608,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.7941483182130646,
+          "pitch": 0.07046878922400879,
+          "rotation": 0,
+          "target": "0-p"
+        },
+        {
+          "yaw": 2.0142814496035717,
+          "pitch": 0.2110818015526803,
+          "rotation": 0,
+          "target": "5--pp"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2--2",
+      "name": "Сокольники 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--1",
+      "name": "Сокольники 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "4---1-2",
+      "name": "Холл Сокольники 1-2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -2.899943776374391,
+        "pitch": -0.05930614778200116,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7157480845731605,
+          "pitch": -0.06663067714732307,
+          "rotation": 0,
+          "target": "2--2"
+        },
+        {
+          "yaw": -0.8848139890339812,
+          "pitch": -0.03719971920327936,
+          "rotation": 0,
+          "target": "3--1"
+        },
+        {
+          "yaw": -1.216470171320843,
+          "pitch": 0.0005789249824701415,
+          "rotation": 0,
+          "target": "5--pp"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5--pp",
+      "name": "Холл Pегистpация",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": -0.1288858524549692,
+        "pitch": -0.03339481093023977,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9227935910228702,
+          "pitch": 0.008827452717740769,
+          "rotation": 0,
+          "target": "4---1-2"
+        },
+        {
+          "yaw": 1.3352341576453508,
+          "pitch": 0.00877536952430269,
+          "rotation": 0,
+          "target": "1--2-"
+        },
+        {
+          "yaw": -1.8742642058150167,
+          "pitch": 0.07807389549041233,
+          "rotation": 0,
+          "target": "6--p"
+        },
+        {
+          "yaw": -2.03999781244309,
+          "pitch": 0.08495593538927082,
+          "rotation": 0,
+          "target": "7-p-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6--p",
+      "name": "Охотный pяд",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3228470375078683,
+          "pitch": -0.050964507471006115,
+          "rotation": 0,
+          "target": "5--pp"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-p-",
+      "name": "Кpымский вал",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1488,
+      "initialViewParameters": {
+        "yaw": 0.013403535761785434,
+        "pitch": 0.04081588002585512,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.048700169390454,
+          "pitch": -0.015291917706028002,
+          "rotation": 0,
+          "target": "5--pp"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
